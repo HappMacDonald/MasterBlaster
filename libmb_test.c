061dd2883024e90535883a883068487f83967de0
Jesse Thompson <jesset@gmail.com>
2021-03-09 10:28:12 -0800

    Further function calling success, including receiving a small struct return value from the assembly function through the registers! hahaha :D

--- libmb_test.c
+++ libmb_test.c
@@ -9,10 +9,18 @@
 // Yes.. percent sign, then break out of string and have whitespace around macro thingy.
 
 int main()
-{ printf("Before\n");
+{ char resultBuffer[20];
+  struct MasterBlasterString actualResults;
+  printf("Before\n");
   putMemoryProcedure("Hello", 4, STDOUT);
   printf("(%c)\n", *TrigentasenaryUppercaseDigits+1);
   printf("(%.*s)\n", 10, (&TrigentasenaryUppercaseDigits));
+  actualResults = unsignedIntegerToStringBase16(0xdeadbeef, resultBuffer);
+  printf("a=%" PRIX64 ", b=%" PRIX64 "\n", actualResults.a, actualResults.b);
+  printf("(%.*s)\n", actualResults.b, actualResults.a);
+  actualResults = unsignedIntegerToStringBase10(0xdeadbeef, resultBuffer);
+  printf("a=%" PRIX64 ", b=%" PRIX64 "\n", actualResults.a, actualResults.b);
+  printf("(%.*s)\n", actualResults.b, actualResults.a);
   printf("After\n");
   return(0); // All clear
 }
